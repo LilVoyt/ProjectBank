6ce6024c2fbe46c982911d0f333c46160b8a2fdf
Voyt <olegvoyt6@gmail.com>
2024-04-22 08:44:07 +0300

    22.04 All Working

---
+++ ProjectBank/Controllers/AccountController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectBank.Controllers
+{
+    public class AccountController : ControllerBase
+    {
+    }
+}
--- ProjectBank/Entities/Account.cs
+++ ProjectBank/Entities/Account.cs
@@ -9,9 +9,8 @@ namespace ProjectBank.Entities
         public string Name { get; set; }
         public double Balance { get; set; }
         //Foreign
-        public Guid CustomerID { get; set; }
         public Guid EmployeeID { get; set; }
-        public Customer Customers { get; set; }
+        public ICollection<Customer> Customers { get; set; }
         public ICollection<Card> Cards { get; set; }
         public ICollection<Employee> Employees { get; set; }
     }
